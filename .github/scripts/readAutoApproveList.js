c1bc175d036b6d3b51bd32ba0e503d0b77f5cc19
Stefan (Shalom) <imnotashrimp@users.noreply.github.com>
2023-06-04 19:59:55 +0300

    test (#6)
    * test
    
    * fix auto-approve
    
    * commit
    
    * fix indentation
    
    * fix negative
    
    * list changed files
    
    * fix list_changed_files outputs
    
    * add changed files lgo
    
    * changed to all_changed_and_modified_files
    
    * change to files_ignore
    
    * auto approve

--- .github/scripts/readAutoApproveList.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const ignore = require('ignore');
-
-const fileContents = fs.readFileSync('./.auto-approve', 'utf8');
-const ig = ignore().add(fileContents);
-
-fs.writeFileSync('./auto-approve.json', JSON.stringify(ig));
